fbd578b375c0ece0a5fd2e564fe61a68c000f516
ombogdan <omelcenko44@gmail.com>
2020-09-06 18:40:28 +0300

    Initial commit

---
+++ Tasks/1/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task2 task = new Task2();
+            task.Main1();
+        }
+    }
+}
